ddaab1aa8a52f9bfe4ce9de67c0a08b2bd3b85ba
Czino <eric@czino.de>
2021-07-01 13:08:05 +0200

    Add debunk as trigger word

--- tweetUtils.mjs
+++ tweetUtils.mjs
@@ -25,7 +25,7 @@ export const getLatestTweets = since_id => {
       if (tweets?.length > 0) {
         tweets = tweets
           .filter(tweet => tweet.id_str > since_id) // failsafe
-          .filter(tweet => /@btcfudbuster help/.test(tweet.text)) // trigger word
+          .filter(tweet => /@btcfudbuster (help|debunk)/.test(tweet.text)) // trigger word
           // .filter(tweet => {
           // this does not work quite as expected, see this message that shouldn't have been posted becaus ethe bot was actually not tagged
           // https://twitter.com/btcfudbuster/status/1410278932562186241
